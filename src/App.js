30757634fafa7759e7a49e5027d2cfd32eab77a7
Mohammad Rabbani <mohammad.rabbani@tigeranalytics.local>
2022-05-01 11:02:43 +0530

    Updated todoList

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
 function App() {
+  const [val, setVal] = useState("");
+  const [word, setWord] = useState([
+    ...JSON.parse(localStorage.getItem("todoList")),
+  ]);
+  const [id, setId] = useState("");
+  const addLetter = (lett) => {
+    setVal(lett.target.value);
+  };
+  const addWord = (e) => {
+    e.preventDefault();
+    const newWordList = word.filter((ele) => ele.id !== id);
+
+    if (val)
+      setWord([
+        ...newWordList,
+        { name: val, id: new Date().getTime().toString() },
+      ]);
+    setVal("");
+  };
+  const deleteWord = () => {
+    word.pop();
+    setWord([...word]);
+  };
+  const deleteAll = () => {
+    setWord([]);
+  };
+  const deleteThis = (id) => {
+    const getWord = word.filter((ele) => ele.id !== id);
+    setWord([...getWord]);
+  };
+  const editThis = (id) => {
+    const getItem = word.find((ele) => ele.id === id);
+    setId(id);
+    setVal(getItem.name);
+  };
+  useEffect(() => {
+    localStorage.setItem("todoList", JSON.stringify(word));
+  }, [word]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <input type="text" onChange={(e) => addLetter(e)} value={val} />
+        <input type="submit" onClick={(e) => addWord(e)} value="Add" />
+        <input type="button" onClick={deleteWord} value="Delete" />
+        <input type="button" onClick={deleteAll} value="Delete All" />
+        {word.map((ele) => {
+          return (
+            <div>
+              <p>{ele.name}</p>
+              <input
+                type="button"
+                value="edit"
+                onClick={() => editThis(ele.id)}
+              />
+              <input
+                type="button"
+                value="Delete"
+                onClick={() => deleteThis(ele.id)}
+              />
+            </div>
+          );
+        })}
+      </form>
     </div>
   );
 }
